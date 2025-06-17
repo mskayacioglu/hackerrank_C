ab3bc29117fc91afb49f4bf16dc6ef2adc21bd5d
Mert Samet Kayacioglu <mskayacioglu@icloud.com>
2025-06-17 15:48:23 +0300

    Solution of the challenge: https://www.hackerrank.com/challenges/sum-numbers-c/problem?isFullScreen=true

--- Introduction/SumAndDifferenceOfTwoNumbers/SumAndDifferenceOfTwoNumbers.c
+++ Introduction/SumAndDifferenceOfTwoNumbers/SumAndDifferenceOfTwoNumbers.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
-#include <string.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    int num_1, num_2;
-    float num_3, num_4;
-    
-    scanf("%d %d", &num_1, &num_2);
-    scanf("%f %f", &num_3, &num_4);
-    
-    printf("%d %d\n", (num_1 + num_2), (num_1 - num_2));
-    printf("%.1f %.1f", (num_3 + num_4), (num_3 - num_4));
-    
-    return 0;
+int main() {
+  int num_1, num_2;
+  float num_3, num_4;
+
+  scanf("%d %d", &num_1, &num_2);
+  scanf("%f %f", &num_3, &num_4);
+
+  printf("%d %d\n", (num_1 + num_2), (num_1 - num_2));
+  printf("%.1f %.1f", (num_3 + num_4), (num_3 - num_4));
+
+  return 0;
 }
